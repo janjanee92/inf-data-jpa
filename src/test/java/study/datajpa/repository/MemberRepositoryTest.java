22160225766de6ca605970f31f8f2808822ab908
rowanlee92 <watrv41@gmail.com>
2021-05-25 15:18:53 +0900

    Add: 사용자 정의 리포지토리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -254,4 +254,9 @@ public class MemberRepositoryTest {
 
     }
 
+    @Test
+    void callCustom() {
+        //  given
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }

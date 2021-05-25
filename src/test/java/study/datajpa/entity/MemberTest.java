16e838c7d0d64dcd28da5d3ffa59287a712c1c6d
rowanlee92 <watrv41@gmail.com>
2021-05-25 16:09:06 +0900

    Add: Auditing

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,14 +1,18 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import study.datajpa.repository.MemberRepository;
+import study.datajpa.repository.TeamRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +23,10 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     void testEntity() {
@@ -67,6 +75,41 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.team = " + member.getTeam());
         }
+
+    }
+    
+    @Test
+    void JpaEventBaseEntity() throws Exception {
+        //  given
+        Member member = new Member("member1");
+        memberRepository.save(member);  //  @PrePersist
+
+        Thread.sleep(1000L);
+        member.setUsername("member2");
+
+        em.flush(); //  @PreUpdate
+        em.clear();
+        //  when
+        Member findMember = memberRepository.findById(member.getId()).orElseThrow();
+
+        //  then
+        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.lastModifiedDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.createdBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.lastModifiedBy = " + findMember.getLastModifiedBy());
+
+        Team team = new Team("teamA");
+        teamRepository.save(team);  //  @PrePersist
+
+        Thread.sleep(2000L);
+        team.setName("teamB");  //  @PreUpdate
+
+        em.flush();
+        em.clear();
+
+        Team findTeam = teamRepository.findById(team.getId()).orElseThrow();
+        System.out.println("team.createdDate = " + findTeam.getCreatedDate());
+        System.out.println("team.lastModifiedDate = " + findTeam.getLastModifiedDate());
 
     }
 }

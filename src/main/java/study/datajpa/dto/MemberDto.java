2e120708c7983a199598c3307f34b296d2bba6bb
rowanlee92 <watrv41@gmail.com>
2021-05-26 10:34:00 +0900

    Add: Web - paging, sort

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -2,6 +2,7 @@ package study.datajpa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 @AllArgsConstructor
@@ -11,4 +12,9 @@ public class MemberDto {
     private String userName;
     private String teamName;
 
+    // Dto 안에서 Member 엔티티를 받는것은 상관없다. 필드로만 사용하지 않으면 된다!
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.userName = member.getUsername();
+    }
 }
